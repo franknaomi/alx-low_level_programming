18556d28cdcb210d684d98542123f2d3210e1acd
franknaomi <franknaomi0@gmail.comcat>
2022-12-09 11:54:48 -0800

    Reverse Alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
