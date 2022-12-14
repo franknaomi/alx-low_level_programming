24fbb751815b0578b6ac21e60454e2edecf00ac4
franknaomi <franknaomi0@gmail.comcat>
2022-12-14 05:22:58 -0800

    Comment out

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,11 +6,13 @@
  */
 int main(void)
 {
+	/**
+	 * int i;
+	 *
+	 * printf("Infinite loop incoming :(\n");
+	 */
 	int i;
 
-	printf("Infinite loop incoming :(\n");
-
-	i = 0;
 	while (i < 10)
 	{
 		putchar(i);
