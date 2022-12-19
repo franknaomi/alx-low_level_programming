dcaeb01b474eb4bf7e5dfae5042bfce6a5cde805
franknaomi <franknaomi0@gmail.comcat>
2022-12-19 12:30:39 -0800

    Half

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,17 +8,43 @@
  */
 int main(void)
 {
-	int pass, sum;
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	srand(time(NULL));
-	sum = 0;
-	while (sum <= 2645)
+	srand(time(0));
+
+	while (sum < 2772)
 	{
-		pass = (rand() % 128);
-		sum += pass;
-		printf("%c", pass);
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
-	printf("%c", 2772 - sum);
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+	printf("%s", password);
 
 	return (0);
 }
